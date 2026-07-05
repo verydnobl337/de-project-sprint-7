d6f45489ab9d17cb1e1b55b0743f293846737f95
Daniil Bondyrev <verydnob@mail.ru>
2026-07-05 12:50:58 +0300

    done city_matcher Step 2 of project

--- src/scripts/geo/city_matcher.py
+++ src/scripts/geo/city_matcher.py
@@ -1,5 +1,63 @@
-import pyspark.sql import functions as F
+from pyspark.sql import functions as F
 from pyspark.sql.window import Window
 from pyspark.sql.functions import broadcast 
 
-from 
+from geo.haversine import Haversine
+
+
+# Создаю класс для привязки событий к ближайшему городу
+class CityMatcher:
+    def __init__(self, events_df, cities_df):
+        self.events_df = events_df
+        self.cities_df = cities_df
+
+    def match(self):
+        """
+        Логика:
+        1) соединяю события и города
+        2) считаю расстояние
+        3) выбираю ближайший город
+        """
+        events = self.events_df.select(
+            F.col("user_id"),
+            F.col("message_id"),
+            F.col("ts"),
+            F.col("lat").alias("event_lat"),
+            F.col("lon").alias("event_lon")
+        )
+        # убираю шафл
+        cities = broadcast(
+            self.cities_df.select(
+                F.col("city"),
+                F.col("lat").alias("city_lat"),
+                F.col("lng").alias("city_lon")
+            )
+        )
+        # соединяю каждое событие с каждым городом
+        joined = events.crossJoin(cities)
+        # считаю расстояние
+        with_distance = joined.withColumn(
+            "distance",
+            Haversine.distance(
+                F.col("event_lat"),
+                F.col("event_lon"),
+                F.col("city_lat"),
+                F.col("city_lon")
+            )
+        )
+        # выбираю ближайший город для каждого события 
+        window = Window.partitionBy('user_id', 'message_id').orderBy(F.col('distance'))
+        
+        result = with_distance.withColumn(
+            'rn',
+            F.row_number().over(window)
+        ).filter(F.col('rn') == 1)
+
+        # результат
+        return result.select(
+            "user_id",
+            "message_id",
+            "ts",
+            "city",
+            "distance"
+        )
